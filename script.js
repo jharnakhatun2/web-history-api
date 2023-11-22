2288f331fa5e2a32c88db8f14ce099f0b7167d5d
jharnakhatun2 <jharnakhatun203@gmail.com>
2023-11-22 19:08:32 +0600

    added web history api exercise

---
+++ script.js
@@ -0,0 +1,14 @@
+function back() {
+    window.history.back();
+}
+
+function forward() {
+    window.history.forward();
+}
+ function go(n) {
+    window.history.go(n);
+ }
+
+ function getHistory() {
+    alert(window.history.length);
+ }
